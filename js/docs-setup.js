4c2301652a8eb7a334a2fc7643699bd2e40ec6f4
Ankit Muchhala <muchhalaankit@gmail.com>
2016-04-15 07:01:50 +0530

    initial commit

---
+++ js/docs-setup.js
@@ -0,0 +1,205 @@
+NG_DOCS={
+  "sections": {
+    "api": "API Documentation"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "daiictSenTeam13App",
+      "shortName": "daiictSenTeam13App",
+      "type": "overview",
+      "moduleName": "daiictSenTeam13App",
+      "shortDescription": "daiictSenTeam13App",
+      "keywords": "api application daiictsenteam13app main module overview"
+    },
+    {
+      "section": "api",
+      "id": "daiictSenTeam13App.controller:AboutCtrl",
+      "shortName": "AboutCtrl",
+      "type": "controller",
+      "moduleName": "daiictSenTeam13App",
+      "shortDescription": "AboutCtrl",
+      "keywords": "aboutctrl api controller daiictsenteam13app"
+    },
+    {
+      "section": "api",
+      "id": "daiictSenTeam13App.controller:ApplicationCtrl",
+      "shortName": "ApplicationCtrl",
+      "type": "controller",
+      "moduleName": "daiictSenTeam13App",
+      "shortDescription": "ApplicationCtrl",
+      "keywords": "api application applicationctrl applied apply attempting bar based called chat chatrooms clicked clicking clicks contact controller cover daiictsenteam13app data database displayed displaying drawer email ends error fields filled function getdata goto including informaton initialises initmaterial input job jobid jobs left letter loads logout logs matrialise member message modules nav navigation note option parameter passed people professor profile prompting redirect redirected resets resetvalues retrieved return returns rooms route scope screen selected session stored string student submit submitapplication submitted toast true unique user valid validate validation variable view"
+    },
+    {
+      "section": "api",
+      "id": "daiictSenTeam13App.controller:ChatCtrl",
+      "shortName": "ChatCtrl",
+      "type": "controller",
+      "moduleName": "daiictSenTeam13App",
+      "shortDescription": "ChatCtrl",
+      "keywords": "active address aligned api argument bar based box button called chat chatctrl chatroom chatrooms clicked clicking clicks controller corresponding course daiictsenteam13app data database deleted determined display displayed drawer email ends enter error fetches function getdata goto history hits identified incremented initialises initmaterial input instantly jobs key kick kicked kickmember leave leaving left left-aligned list load logged logout logs materialize meesages member memberkey members memebers memers message messages modules nav navigation note onenterhit option owner parameter passed people professor profile redirect redirected redirects remaining return right- room rooms route scope screen send sender sentmessage session slots store stored string student success takes text toast unique user users variable view viewmemberprofile wishes"
+    },
+    {
+      "section": "api",
+      "id": "daiictSenTeam13App.controller:ChatroomsCtrl",
+      "shortName": "ChatroomsCtrl",
+      "type": "controller",
+      "moduleName": "daiictSenTeam13App",
+      "shortDescription": "ChatroomsCtrl",
+      "keywords": "$scope active allowed api applied bar based belong belonging called chat chatroom chatrooms chatroomsctrl check checks clicked clicking code controller corresponding created creating creator creators current daiictsenteam13app data database databse day delay described details displayed displays drawer ends ensure enter error false full function getdata goto greater identify initialises initmaterial inputs jobs join key left lesser list logout logs matrialise mechanism members message method modules moment nav navigation network note object open openchatroom option passed people preferences professor profile queries redirect redirected relevant retrieves return returned returns room rooms rooom scope screen session set slot start stored string student takes time toast transactions true unique updations user valid validate validated validating validation variable view wrong"
+    },
+    {
+      "section": "api",
+      "id": "daiictSenTeam13App.controller:CreatechatCtrl",
+      "shortName": "CreatechatCtrl",
+      "type": "controller",
+      "moduleName": "daiictSenTeam13App",
+      "shortDescription": "CreatechatCtrl",
+      "keywords": "$scope active allowed api applied asks bar based belong belonging button called chat chatroom chatroomid chatrooms check checks clicked clicking clicks code controller corresponding create createchatctrl createchatroom created createdby creates creating creation creator creators current currently daiictsenteam13app data database databse day delay delete deletechatroom deleted deletion described description details displayed displays doesn drawer ends ensure enter entry error exist false fields filled full function getdata goto greater hasn identified identifies identify informed initialises initmaterial input inputs jobs join key left lesser list logged logout logs matrialise maximum mechanism members message method modules moment nav navigation network note number object open openchatroom option particualr passed people preferences professor profile prompted queries redirect redirected resets resetvalues retrieves return returned returns room rooms rooom routr scope screen session set showallchatrooms slot slots start starttime stored string student successful takes thr time toast transactions true unique updations user valid validate validated validating validation variable view wrong"
+    },
+    {
+      "section": "api",
+      "id": "daiictSenTeam13App.controller:FacultyCtrl",
+      "shortName": "FacultyCtrl",
+      "type": "controller",
+      "moduleName": "daiictSenTeam13App",
+      "shortDescription": "StudentCtrl",
+      "keywords": "accepted active api applications bar based button called chat chatroom chatroomid chatrooms clicked clicking clicks controller corresponding created createdjobs daiictsenteam13app database delete deletechatroom deleted deletejob deletion display displayed displaying doesn drawer ends entry error exist faculty function getdata goto hasn html identified identifies identify informed initialises initmaterial input job jobid jobs join key left list loaded logout logs matrialise members message modules nav navigation note object objects open openchatroom option parameter particualr passed people professor profile prompted redirect redirected redirects rejected return room rooms route scope screen session showallchatrooms showalljobs stored string student studentctrl successful takes thr toast unique user variables view viewjob"
+    },
+    {
+      "section": "api",
+      "id": "daiictSenTeam13App.controller:JobsCtrl",
+      "shortName": "JobsCtrl",
+      "type": "controller",
+      "moduleName": "daiictSenTeam13App",
+      "shortDescription": "JobsCtrl",
+      "keywords": "active api applied apply applyforjob bar based called case chat chatrooms checks clicked clicking clicks controller corresponding daiictsenteam13app database displayed displaying drawer ends entry function getdata goto hasn identifies initialises initmaterial input job jobid jobname jobs jobsctrl left list load logout logs matrialise message modules nav navigation note object option parameters passed people professor profile purpose redirect redirected resume return rooms route scope screen session statament stored string student takes toast unique upload user variable variables ve view"
+    },
+    {
+      "section": "api",
+      "id": "daiictSenTeam13App.controller:MainCtrl",
+      "shortName": "MainCtrl",
+      "type": "controller",
+      "moduleName": "daiictSenTeam13App",
+      "shortDescription": "Controller for the route /. It checks authenticates the user",
+      "keywords": "$rootscope api appropriate attaches authenticates authentication browser call calls case checks components controller daiictsenteam13app data email error faculty fetches firebase function gotoregister gotoreset identifies initializes initmaterial input local login main making materialize password raises redirects redirectuser registration request return route scope sends sets storage student successful temporary toats type user usertype validate validates variable"
+    },
+    {
+      "section": "api",
+      "id": "daiictSenTeam13App.controller:PeopleCtrl",
+      "shortName": "PeopleCtrl",
+      "type": "controller",
+      "moduleName": "daiictSenTeam13App",
+      "shortDescription": "PeopleCtrl",
+      "keywords": "address api argument bar based called chat chatrooms clicked clicking clicks controller daiictsenteam13app displayed displaying drawer email ends function getdata goto jobs left list load logged logout logs nav navigation note object option parameter passed people peoplectrl portal professor profile redirect redirected redirects registered return rooms route scope screen session stored string student takes user users variable variables view viewprofile wishes"
+    },
+    {
+      "section": "api",
+      "id": "daiictSenTeam13App.controller:PostingCtrl",
+      "shortName": "PostingCtrl",
+      "type": "controller",
+      "moduleName": "daiictSenTeam13App",
+      "shortDescription": "PostingCtrl",
+      "keywords": "api bar based button called chat chatrooms check checks clicked clicking clicks controller corresponding create created createdjobs createjob creating daiictsenteam13app data database deadline delete deleted deletejob deletion displayed displaying doesn drawer ends entry error exist faculty false fields filled form fulfilled function getdata goto greater hasn html identified identifies informed initialises initmaterial input job jobid jobs left list load loaded logout logs lower matrialise message modules nav navigation note object option passed people performs posting postingctrl professor profile prompted redirect redirected reset resets resetvalues return returned returns rooms route scope screen session showall start stored string student successful toast today true unique user valid validate validating validation variables view"
+    },
+    {
+      "section": "api",
+      "id": "daiictSenTeam13App.controller:ProfileCtrl",
+      "shortName": "ProfileCtrl",
+      "type": "controller",
+      "moduleName": "daiictSenTeam13App",
+      "shortDescription": "ProfileCtrl",
+      "keywords": "altered api argument bar based baswd box called case changing chat chatrooms clicked clicking clicks content controller corresponding daiictsenteam13app data database delete details displayed drawer edits ends error example field fields function getdata goto identify initialises initmaterial institute interest interests jobs key left loaded location logged logout logs matrialise message modules nav navigation note option passed people professor profile profilectrl publication publications redirect redirected remove removeitem removes retrieved retrieves return rooms scope screen searches session stored stores string student switch takes text thekey thr toast triggered type unique updated updateprofile updates user variables view wished wishes"
+    },
+    {
+      "section": "api",
+      "id": "daiictSenTeam13App.controller:RegisterCtrl",
+      "shortName": "RegisterCtrl",
+      "type": "controller",
+      "moduleName": "daiictSenTeam13App",
+      "shortDescription": "Controller of the daiictSenTeam13App",
+      "keywords": "ac account address alerting api appropriate attempting called case check checks clicks conditions confirm confirmation controller created daiict daiictsenteam13app data database displayed domain email emailid error false fields filled format function input inputs login message occur passes password passworf performed portal profile raises register registered registration reset resets resetvalues return returned returns sendpassword sends sign signup stored temporary test toast toasts true user valid validate validation view"
+    },
+    {
+      "section": "api",
+      "id": "daiictSenTeam13App.controller:ResetpasswordCtrl",
+      "shortName": "ResetpasswordCtrl",
+      "type": "controller",
+      "moduleName": "daiictSenTeam13App",
+      "shortDescription": "Controller for ResetpasswordCtrl and there is",
+      "keywords": "api controller daiictsenteam13app resetpasswordctrl"
+    },
+    {
+      "section": "api",
+      "id": "daiictSenTeam13App.controller:StudentCtrl",
+      "shortName": "StudentCtrl",
+      "type": "controller",
+      "moduleName": "daiictSenTeam13App",
+      "shortDescription": "StudentCtrl",
+      "keywords": "active allowed api applications applied apply applyforjob arr bar based belong belonging called case chat chatroom chatrooms checks clicked clicking clicks code controller corresponding creating creator creators current daiictsenteam13app data database databse day delay described displayed displaying drawer ends ensure enter entry false full function getdata goto greater hasn identifies identify initialises initmaterial input inputs job jobid jobname jobs join key left lesser list load logout logs matrialise mechanism members message modules moment nav navigation network note object open openchatroom option parameters passed people preferences professor profile purpose redirect redirected resume return returned returns room rooms rooom route scope screen session set slot start statament stored string student studentctrl submitted takes time toast transactions true unique updations upload user validate validated validating validation variables ve view wrong"
+    },
+    {
+      "section": "api",
+      "id": "daiictSenTeam13App.controller:ViewjobCtrl",
+      "shortName": "ViewjobCtrl",
+      "type": "controller",
+      "moduleName": "daiictSenTeam13App",
+      "shortDescription": "ViewjobCtrl",
+      "keywords": "accept acceptapplication api application applicationid applications bar based called chat chatrooms clicked clicking clicks controller corresponding daiictsenteam13app database displayed drawer ends error fails function generated getdata goto html identify initialises initmaterial job jobid jobs key left load logout logs matrialise message modules nav navigation note notification option parameter particualr passed people professor profile redirect redirected reject rejectapplication return rooms route scope screen searching session status stored string student toast unique updated updation user variable view viewjobctrl"
+    },
+    {
+      "section": "api",
+      "id": "daiictSenTeam13App.controller:ViewprofileCtrl",
+      "shortName": "ViewprofileCtrl",
+      "type": "controller",
+      "moduleName": "daiictSenTeam13App",
+      "shortDescription": "ViewprofileCtrl",
+      "keywords": "api associated bar based called chat chatrooms clicked clicking controller daiictsenteam13app data database displayed drawer email ends error function getdata goto initialises initmaterial institute interests jobs left loaded logged logout logs matrialise message modules nav navigation note option parameter passed people professor profile publications redirect redirected retrieved retrieves return rooms route scope screen session stores string student user variables view viewprofilectrl wishes"
+    },
+    {
+      "section": "api",
+      "id": "daiictSenTeam13App.filter:searchChat",
+      "shortName": "searchChat",
+      "type": "filter",
+      "moduleName": "daiictSenTeam13App",
+      "shortDescription": "searchChat",
+      "keywords": "api daiictsenteam13app filter searchchat"
+    },
+    {
+      "section": "api",
+      "id": "daiictSenTeam13App.filter:searchChatRoom",
+      "shortName": "searchChatRoom",
+      "type": "filter",
+      "moduleName": "daiictSenTeam13App",
+      "shortDescription": "searchChatRoom",
+      "keywords": "api daiictsenteam13app filter searchchatroom"
+    },
+    {
+      "section": "api",
+      "id": "daiictSenTeam13App.filter:searchJobs",
+      "shortName": "searchJobs",
+      "type": "filter",
+      "moduleName": "daiictSenTeam13App",
+      "shortDescription": "searchJobs",
+      "keywords": "api daiictsenteam13app filter searchjobs"
+    },
+    {
+      "section": "api",
+      "id": "daiictSenTeam13App.filter:searchPeople",
+      "shortName": "searchPeople",
+      "type": "filter",
+      "moduleName": "daiictSenTeam13App",
+      "shortDescription": "searchPeople",
+      "keywords": "api daiictsenteam13app filter searchpeople"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "html5Mode": false,
+  "editExample": true,
+  "startPage": "/api",
+  "scripts": [
+    "angular.min.js"
+  ]
+};
